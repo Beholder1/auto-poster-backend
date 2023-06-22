3bec6df5beec245bc53fb2fedb69f1e9a8d7862e
Kamil Włodarczyk <kamil.limanowa@gmail.com>
2023-06-22 23:34:32 +0200

    Fixed location methods

--- src/main/java/com/example/autoposterbackend/AutoPosterBackendApplication.java
+++ src/main/java/com/example/autoposterbackend/AutoPosterBackendApplication.java
@@ -6,8 +6,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class AutoPosterBackendApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(AutoPosterBackendApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(AutoPosterBackendApplication.class, args);
+    }
 
 }
--- src/main/java/com/example/autoposterbackend/entity/Location.java
+++ src/main/java/com/example/autoposterbackend/entity/Location.java
@@ -10,16 +10,17 @@ import lombok.Setter;
 @Table(name = "locations")
 public class Location {
     @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     @Column(name = "id")
     private Integer id;
 
     @Column(name = "name")
     private String name;
 
-    @Column(name = "user_id")
+    @Column(name = "user_id", insertable = false, updatable = false)
     private Integer userId;
 
     @ManyToOne
-    @JoinColumn(name = "user_id", insertable = false, updatable = false)
+    @JoinColumn(name = "user_id")
     private User user;
 }
